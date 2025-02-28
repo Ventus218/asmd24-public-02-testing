44777aed2cb3910f31270f66ed633aac66837fd4
Alessandro Venturini - alessandro.venturin6@studio.unibo.it <alessandro.venturin6@studio.unibo.it>
2025-02-28 12:51:50 +0100

    fix random Failing policy

--- src/main/java/devices/RandomFailing.java
+++ src/main/java/devices/RandomFailing.java
@@ -1,12 +1,13 @@
 package devices;
 
 import java.util.Random;
+import java.util.random.RandomGenerator;
 
 public class RandomFailing implements FailingPolicy {
-	private Random random;
+	private RandomGenerator random;
 	private boolean failed = false;
 
-	void RandomFailing(Random random) {
+	void RandomFailing(RandomGenerator random) {
 		this.random = random;
 	}
 
